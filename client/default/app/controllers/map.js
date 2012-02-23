a531632d8010350068a38af532bff00f3d5b7863
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-23 20:07:03 +0000

    add map logic

---
+++ client/default/app/controllers/map.js
@@ -0,0 +1,58 @@
+var mapController = {
+	markers : [], // Keep track of any map markers
+	map : null,
+	renderMap : function() {
+		var that=this;
+		if($("#mapCanvas").children().length == 0) {
+			$("#mapCanvas").height($(window).height() - $(".header").height());
+			var myOptions = {
+				center : new google.maps.LatLng(-34.397, 150.644),
+				zoom : 8,
+				mapTypeId : google.maps.MapTypeId.ROADMAP
+			};
+			this.map = new google.maps.Map(document.getElementById("mapCanvas"), myOptions);
+			var map=this.map;
+			this.getLocation();
+			mapModel.loadPoints(function(res) {
+				for(var i = 0; i < res.data.locations.length; i++) {
+					var point = res.data.locations[i];
+					var pos = new google.maps.LatLng(point.lat, point.lon);
+					that.markers.push(new google.maps.Marker({
+						position : pos,
+						map : map,
+						icon : 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + (i + 1) + '|FF0000|000000'
+					}));
+				}
+
+			});
+			
+		}
+	},
+	/*
+	 * Get the users location and draw a marker at their location
+	 */
+	getLocation : function() {
+		// Instance of the google map
+		var map = this.map;
+		var pos = {};
+		var that=this;
+		$fh.geo({
+			interval : 0
+		}, function(res) {
+			pos = new google.maps.LatLng(res.lat, res.lon);
+			map.setCenter(pos);
+
+			// Create a marker at the current location
+			that.markers.push(new google.maps.Marker({
+				position : pos,
+				map : map,
+				icon : 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=0|00FF00|000000'
+			}));
+
+		}, function() {
+			// We failed to get the users geolocation, fallback to geo ip
+			alert("$fh.geo failed");
+			alert(JSON.stringify(res.geoip));
+		});
+	}
+};
